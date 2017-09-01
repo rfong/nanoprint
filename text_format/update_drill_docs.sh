fadb12cb4adfaedc0742efe562517080f61d1776
rfong <rachefo@gmail.com>
2017-09-01 03:26:58 +0000

    updated drill docs

--- text_format/update_drill_docs.sh
+++ text_format/update_drill_docs.sh
@@ -3,5 +3,11 @@
 python print_friendly_csv.py \
   --header="### metric clearance drills ###\nmeasurements in millimeters" \
   --col-widths=5,10,12 \
-  documents/drill_clearance_metric.csv \
-  documents/drill_clearance_metric.txt
+  docs/drill_clearance_metric.csv \
+  docs/drill_clearance_metric.txt
+
+python print_friendly_csv.py \
+  --header="### US clearance drills ###\nmeasurements in inches" \
+  --col-widths=5,10,12 \
+  docs/drill_clearance_us.csv \
+  docs/drill_clearance_us.txt
